eef81de90e37787c5b8b645245b16171bb292c37
josie113 <97979721+josie113@users.noreply.github.com>
2022-06-17 15:04:55 -0700

    fixed typo/debugged

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -15,9 +15,10 @@ export default function FormattedDate(props) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
-    minutes = `0{minutes}`;
+    minutes = `0${minutes}`;
   }
   return (
     <div>
