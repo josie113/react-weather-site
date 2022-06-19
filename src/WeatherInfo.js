e5d1c1e50efbe5c1057378149814a09f638f920d
josie113 <97979721+josie113@users.noreply.github.com>
2022-06-18 17:18:15 -0700

    updated icons to react icons, connected to api

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -26,11 +27,7 @@ export default function WeatherInfo(props) {
             </div>
             <br />
             <div className="clearfix weather-temperature">
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="icon"
-              />
+              <WeatherIcon code={props.data.icon} />
               <h2 className="text-capitalize">{props.data.description}</h2>
             </div>
           </div>
