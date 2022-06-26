2fcee5e6525ce888df66bd2dc7ea0bd11ad558bc
josie113 <97979721+josie113@users.noreply.github.com>
2022-06-25 23:19:25 -0700

    added props and useState for daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,41 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  console.log(props);
 
-  let apiKey = "774e0d8fffbeeedfdccb46cff718bbcf";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row gx-1 mb-3">
-        <div className="col-4 p-2" id="daily-forecast">
-          <div className="daily">
-            <section className="tuesday">
-              <div className="row">
-                <div className="col-2 text-left">
-                  <span className="tues">TUE</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row gx-1 mb-3">
+          <div className="col-4 p-2" id="daily-forecast">
+            <div className="daily">
+              <section className="tuesday">
+                <div className="row">
+                  <div className="col-2 text-left">
+                    <span className="tues">{forecast[0].dt}</span>
+                  </div>
+                  <div className="col-10 text-right mt-2">
+                    <span className="day-2">
+                      <WeatherIcon
+                        code={forecast[0].weather[0].icon}
+                        size={40}
+                      />
+                      <span className="tues-high">{forecast[0].temp.max}°</span>{" "}
+                      |<span className="tues-low">{forecast[0].temp.min}°</span>
+                    </span>
+                  </div>
                 </div>
-                <div className="col-10 text-right mt-2">
-                  <span className="day-2">
-                    <WeatherIcon code="10d" size={40} />
-                    <span className="tues-high">48°</span> |
-                    <span className="tues-low">42°</span>
-                  </span>
-                </div>
-              </div>
-            </section>
+              </section>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "774e0d8fffbeeedfdccb46cff718bbcf";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
